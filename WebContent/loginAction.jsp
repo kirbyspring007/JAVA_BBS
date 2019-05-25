da347ce0a2a704707f74fc11f62d87a75b9c1802
JEON YONGSUK <49046888+kirbyspring007@users.noreply.github.com>
2019-05-25 19:40:22 +0900

    2019/05/25

--- WebContent/loginAction.jsp
+++ WebContent/loginAction.jsp
@@ -3,7 +3,7 @@
  <%@ page import="user.UserDAO" %>
  <%@ page import="java.io.PrintWriter" %> 
  <% request.setCharacterEncoding("UTF-8"); %>
- <jsp.useBean id="user" class="user.User" scope="page" />
+ <jsp:useBean id="user" class="user.User" scope="page" />
  <jsp:setProperty name="user" property="userID" />
  <jsp:setProperty name="user" property="userPassword" />
 <!DOCTYPE html>
--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -14,10 +14,10 @@ public class UserDAO {
 	
 	public UserDAO() {
 		try {
-			String dbURL = "jdbc:mysql://localhost:8080/BBS";
+			String dbURL = "jdbc:mysql://localhost:8080/bbs";
 			String dbID = "root";
-			String dbPassword = "kudo0612";
-			Class.forName("com.mysql.jdbc.Drvier");
+			String dbPassword = "1234";
+			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -25,11 +25,11 @@ public class UserDAO {
 	}
 	
 	public int login(String userID, String userPassword) {
-		String SQL = "SELRCT userPassword FORM USER WHERE userID = ?";
+		String SQL = "SELECT userPassword FORM USER WHERE userID = ?";
 		try {
 			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, userID);
-			rs  = pstmt.executeQuery();
+			rs = pstmt.executeQuery();
 			if (rs.next()) {
 				if(rs.getString(1).equals(userPassword)) {
 					return 1; //ログイン成功
